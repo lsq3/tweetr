8483c462555f69e436fe2de82feeb41f3f106733
Sean <leoseanquilty@gmail.com>
2017-08-08 22:22:25 +0000

    Created logic to count the characters typed into the textbox, including logic to add/remove a class to the counter field, which will be leveraged in CSS

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,2 +1,23 @@
+"use strict";
+
 // code pertinent to counting the number of characters typed into the tweet textbox
 
+$(document).ready(function() {
+
+  $("textarea").on('input', function() {
+
+    const charCount = 140 - $(this).val().length
+    $("#char-counter").html(charCount);
+
+    if (charCount < 0) {
+      $("#char-counter").removeClass("within-limit")
+      $("#char-counter").addClass("beyond-limit")
+    } else if (charCount >= 0) {
+      $("#char-counter").removeClass("beyond-limit")
+      $("#char-counter").addClass("within-limit")
+
+    }
+
+  });
+
+});
