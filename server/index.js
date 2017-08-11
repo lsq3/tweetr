ac647d6d876a02b22be1b49f52c8b6469fc05f46
Sean <leoseanquilty@gmail.com>
2017-08-11 16:20:48 +0000

    Successfully implemented MongoDB to ensure tweet data persists

--- server/index.js
+++ server/index.js
@@ -10,8 +10,20 @@ const app           = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// The in-memory database of tweets. It's a basic object with an array in it.
-const db = require("./lib/in-memory-db");
+//Hooking into a MongoDB connection
+const MongoClient   = require("mongodb").MongoClient;
+const MONGODB_URI   = "mongodb://localhost:27017/tweeter";
+
+//Connecting to MongoDB
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+//if we encounter an error when connecting, relay that to the user
+    console.log(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
+
+//Otherwise, we've connected successfully to MongoDB
+console.log(`Connected to MongoDB: ${MONGODB_URI}`);
 
 // The `data-helpers` module provides an interface to the database of tweets.
 // This simple interface layer has a big benefit: we could switch out the
@@ -28,6 +40,8 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
+
+});
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
